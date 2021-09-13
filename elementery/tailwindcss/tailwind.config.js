7ad9d786277818115402c19853c9def3cf137c6d
Quinnio <quynhnguyen.biz@gmail.com>
2021-09-13 08:36:48 +0700

    update buoi 3 va tailwindcss

--- elementery/tailwindcss/tailwind.config.js
+++ elementery/tailwindcss/tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   mode: 'jit',
   purge: [
@@ -5,10 +7,39 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    divideWidth: {
+      DEFAULT: '1px',
+      '0': '0',
+      '2': '2px',
+     '3': '3px',
+      '4': '4px',
+     '6': '6px',
+     '8': '8px',
+    },
+    colors: {
+      "quinnio-blue": "#458FF6",
+      "quinnio-gray": "#7D7987",
+      "quinnio-white": "#FFFFFF",
+      "quinnio-black": "#1F1534",
+    },
     extend: {},
+    fontFamily: {
+      Mulish: ["Mulish", "sans-serif"], 
+    },
+    container: {
+      center:true,
+      padding: "1rem",
+      screen: {
+        lg: "1124px",
+        xl: "1124px",
+        "2xl": "1124px",
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      animation: ['motion-safe'],
+    },
   },
   plugins: [],
 }
